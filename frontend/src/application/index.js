ece4d9e6aeb7c161333b435a8f09c016aff417bd
Rasul Kireev <me@rasulkireev.com>
2024-05-25 13:52:35 +0300

    mvp

--- frontend/src/application/index.js
+++ frontend/src/application/index.js
@@ -1,1 +1,8 @@
 import "../styles/index.css";
+
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
+
+window.Stimulus = Application.start();
+const context = require.context("../controllers", true, /\.js$/);
+window.Stimulus.load(definitionsFromContext(context));
